ca22b2a680ca674df6314f044be5c6f11ff6c21e
Youri Cadet <cadetjesusyouri@gmail.com>
2020-08-05 23:46:16 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,2 +1,9 @@
-def using_untif 
-  
+def using_until
+  levitation_force = 6
+  #your code here
+  until levitation_force ==10
+    puts "Wingardium Leviosa"
+    levitation_force +=1
+  end
+
+end
